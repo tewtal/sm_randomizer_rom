30769cda23b0a5b92efbc4b558739cfc9c9213c4
tewtal <total@viskos.org>
2020-03-01 22:30:53 +0100

    Initial import

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo Building Super Metroid Randomizer
+
+cd resources
+python3 create_dummies.py 00.sfc ff.sfc
+./asar --no-title-check --symbols=wla --symbols-path=../build/sm.sym ../src/main.asm 00.sfc
+./asar --no-title-check --symbols=wla --symbols-path=../build/sm.sym ../src/main.asm ff.sfc
+python3 create_ips.py 00.sfc ff.sfc sm.ips
+rm 00.sfc ff.sfc
+
+for p in ../src/randopatches/ips/*.ips; do python3 merge_ips.py $p sm.ips; done
+
+cp sm.ips ../build/sm.ips > /dev/null
+cd ..
+echo Done
---
+++ src/config.asm
@@ -0,0 +1,3 @@
+org $CEFF00
+config_multiworld:
+    dw $0000
