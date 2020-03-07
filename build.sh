7955aea7c7e11ee73a16482763ea316aa63bd5ca
tewtal <total@viskos.org>
2020-03-07 23:43:26 +0100

    Custom sprites

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# Remember to commit an updated build.bat as well if making changes
 
 echo Building Super Metroid Randomizer
+
+find . -name build.py -exec python3 {} \;
 
 cd resources
 python3 create_dummies.py 00.sfc ff.sfc
@@ -9,8 +12,9 @@ python3 create_dummies.py 00.sfc ff.sfc
 python3 create_ips.py 00.sfc ff.sfc sm.ips
 rm 00.sfc ff.sfc
 
-for p in ../src/randopatches/ips/*.ips; do python3 merge_ips.py $p sm.ips; done
+for f in ../src/randopatches/ips/*.ips; do python3 merge_ips.py $f sm.ips; done
 
 cp sm.ips ../build/sm.ips > /dev/null
+
 cd ..
 echo Done
