e973f345989e7e7f70711f7ed93c1de96e0f2a22
Rebelus Quo <rebelusquo@tutanota.com>
2020-04-11 03:54:30 +0200

    Correct a pose triggered by turning under water physics
    With Screw attack, or Screw attack and Space jump equipped, trying to turn under the effect of water physics (in or into water without Gravity equipped) would trigger a static frame from the Space jump spin animation. This commit fixes this and makes the following changes of importance:
    
    - The gun port table is moved and starts at $90:86AF .
    - The address for the screw attack behavior flag is moved to $9B:93FE
      - Since this is at the end of space for the former vanilla death animation poses, the flag is now specified in src/sm/sprite/config.asm rather than src/sm/config.asm .
      - Given that this is in the same bank as of one of the routines, we explicitly use the `LDA.l` opcode to make sure the long version is used.
    - The long version of `LDA` and `STA` opcodes are also used for timing neutrality between branches that pertain to the effect of the screw attack flag.

--- src/config.asm
+++ src/config.asm
@@ -1,14 +1,8 @@
 org $CEFF00
 config_flags:
 ; Multiworld enabled 
-config_multiworld: ; $CEFF00
+config_multiworld:  ; $CEFF00
     dw $0000
 ; Custom sprite used
 config_sprite: ; $CEFF02
     dw $0000
-
-
-; Custom sprite engine flags
-org $B4F500
-config_screwattack: ; $B4F500
-    dw #$0000
